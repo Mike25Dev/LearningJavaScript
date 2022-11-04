0539444300553aa61940f27f09709c08e32d5f49
ntt-michael <michael.urrego@netto.com.co>
2022-11-04 16:53:19 -0500

    JavaScript

---
+++ JS-Basico/funciones.js
@@ -0,0 +1,14 @@
+// Funciones declarativas
+
+function miFuncion(){
+    return 3;
+}
+miFuncion();
+
+//Funcion de expresion
+
+var miFuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion();
---
+++ JS-Basico/variables.js
@@ -0,0 +1,17 @@
+var nombre = "Mike";
+
+// declarar variable
+var edad;
+// inicializar variable
+edad = 30;
+edad
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Mike",
+    edad: 25
+}
+
+persona;
+
